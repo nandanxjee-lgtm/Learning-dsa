0631d5fce3520cb53545a1adb8125267824fce57
nandanxjee-lgtm <nandanxjee@gmail.com>
2026-04-15 11:56:28 +0530

    made a function that can change your binary numbers into decimal number

--- code.cpp
+++ code.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int binary(int n){
+int dec2binary(int n){
     int ans=0, pow=1,rem;
     for(int i=0;n>0;n/=2){
             rem=(n%2)*pow;
@@ -10,8 +10,16 @@ int binary(int n){
     }
     return ans;
 }
+int binary2dec(int n){
+    int pow=1,ans=0;
+    for(int i=0;n>0;n/=10){
+        ans+=(n%10)*pow;
+        pow*=2;   
+    }
+    return ans;
+}
 int main(){
-    cout<<binary(234)<<endl;
+    cout<<binary2dec(110010)<<endl;
     return 0;
 }
 
